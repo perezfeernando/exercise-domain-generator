c0a5ad4c3016697890e87be1ed785631b40c1bd8
Fernando Pérez <fernandoperezdeveloper@gmail.com>
2021-05-24 18:43:03 +0000

    Nuevo ejercicio domain

--- src/app.js
+++ src/app.js
@@ -9,3 +9,23 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["El", "La"];
+let adj = ["Bien", "Grande"];
+let noun = ["Animal", "Estadio"];
+let extension = [".com", ".es"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < extension.length; l++) {
+        let newDomain = pronoun[i] + adj[j] + noun[k] + extension[l];
+        let lista = document.getElementById("domain"); //Buscar el elemento con la id dada
+        let newLi = document.createElement("li"); //crear nuevo elemento
+        let contenido = document.createTextNode(newDomain); //crea texto
+        lista.appendChild(newLi); //añade newLi a lista
+        newLi.appendChild(contenido); // añade texto al nuevo newli
+      }
+    }
+  }
+}
